01982817019308d83115fc31b67dde2960126f80
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-02-15 21:36:00 -0300

    File FileInfo and IOException

--- WorkingWithFiles/WorkingWithFiles/Program.cs
+++ WorkingWithFiles/WorkingWithFiles/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace WorkingWithFiles
+﻿using System;
+using System.IO;
+
+namespace WorkingWithFiles
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string sourcePath = @"C:\temp\file1.txt";
+            string targetPath = @"C:\temp\file2.txt";
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An issue occured.");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
