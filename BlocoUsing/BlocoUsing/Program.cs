e7d4a996b627c36d6b53e7cc1941a71a871b919c
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-02-16 13:06:49 -0300

    Aprendendo o bloco using

---
+++ BlocoUsing/BlocoUsing/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BlocoUsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file1.txt";
+
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine("An issue occurred.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+} 
