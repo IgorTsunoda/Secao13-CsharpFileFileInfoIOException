1bac9820aee9cba5bfa146f6ccad3c5fa8ff2a96
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-02-15 22:04:15 -0300

    FileStream and StreamReader

---
+++ FileStram_StreamReader/FileStram_StreamReader/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace FileStram_StreamReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An issue occurred.");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
