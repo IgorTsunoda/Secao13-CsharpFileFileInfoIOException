2a06fa647f7dbdcb66295420f28388328ceda96d
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-02-18 19:54:03 -0300

    Directory and DirectoryInfo

---
+++ DirectoryDirectoryInfo/DirectoryDirectoryInfo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryDirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string element in folders)
+                {
+                    Console.WriteLine(element);
+                }
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine();
+                Console.WriteLine("FILES");
+                foreach (string element in files)
+                {
+                    Console.WriteLine(element);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder");
+
+            }
+            catch (IOException e) 
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
